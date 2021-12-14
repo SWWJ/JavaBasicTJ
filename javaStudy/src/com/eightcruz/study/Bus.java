500cb63f89ee017c55d6e4c10a39c6733fc18260
SWWJ <swjoo1229@gmail.com>
2021-12-14 13:11:03 +0900

    d

--- javaStudy/src/com/eightcruz/study/Bus.java
+++ javaStudy/src/com/eightcruz/study/Bus.java
@@ -5,7 +5,7 @@ public class Bus extends Car {
 	int fee;
     public Bus(String name, int number, int fee) {
         // super를 이용해 Car 클래스의 생성자를 이용하세요.
-        super();
+        super(name,number); // 부모 class인 Car에 들어가야 하는 부모의 name, number 를 호출
         this.fee = fee;
     }
 }
